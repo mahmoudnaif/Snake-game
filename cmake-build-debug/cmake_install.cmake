90d4d884f4ca00cfcc1c53c81d8ff9fee69bb921
mahmoudnaif <mahmoudnaif788@gmail.com>
2024-04-16 09:58:12 +0200

    Added wall sections and now you can cutsom make levels using txt files to do: 1- adding texture to the game 2- adding text 3- allowing the user to make his own levels (without the need of doing it manually using txt files) (pusing to github)

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/My Projects/hello sfml
+# Install script for directory: C:/My Projects/Snake game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/My Projects/hello sfml/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/My Projects/Snake game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
